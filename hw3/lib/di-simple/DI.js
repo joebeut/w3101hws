e010dea61437ead6a3dd995781059130b5f7c0ae
Joe Beutler <jtb2133@columbia.edu>
2015-02-28 23:54:16 -0500

    feat(DI.js): my best attempt. I cant make the tests work but I swear the code should be good

--- hw3/lib/di-simple/DI.js
+++ hw3/lib/di-simple/DI.js
@@ -1,24 +1,28 @@
-(function () {
+var module = (function () {
   'use strict';
 
-  var DI = function (dependency) {
-    this.dependency = dependency;
+  var dependencies = {};
+
+  var inject = function (target) {
+    var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+    var FN_ARG_SPLIT = /,/;
+    var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+    var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+    var text = target.toString();
+    var args = text.match(FN_ARGS)[1].split(',');
+
+    target.apply(target, this.getDependencies(args));
   };
 
-  function inject (func) {
-    var argsString = fn.toString()
-      .match(/^function\s*[^\(]*\(([^\)]*)\)/)[1];
-    var argNames = argsString.split(',').map(function(argName) {
-      return argName.replace(/\s*/g, '');
+  var getDependencies = function(arr) {
+    var self = this;
+    return arr.map(function(value) {
+      return self.dependencies[value];
     });
-    var args = argNames.map(function(argName) {
-      return cache[argName];
-    });
-    return fn.apply(null, args);
   };
 
-  function register (name, value) {
-    DI[name] = value;
+  var register = function (name, dependency) {
+    dependencies[name] = dependency;
   };
 
   module.exports = {
