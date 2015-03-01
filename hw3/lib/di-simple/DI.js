f985d08747e969427969e8cd7cb159ce73e1b7cb
Joe Beutler <jtb2133@columbia.edu>
2015-02-28 22:21:01 -0500

    feat(DI.js): modified di simple to include inject functionality

--- hw3/lib/di-simple/DI.js
+++ hw3/lib/di-simple/DI.js
@@ -1,6 +1,28 @@
 (function () {
   'use strict';
 
-  module.exports = {};
+  var DI = function (dependency) {
+    this.dependency = dependency;
+  };
 
+  function inject (func) {
+    var argsString = fn.toString()
+      .match(/^function\s*[^\(]*\(([^\)]*)\)/)[1];
+    var argNames = argsString.split(',').map(function(argName) {
+      return argName.replace(/\s*/g, '');
+    });
+    var args = argNames.map(function(argName) {
+      return cache[argName];
+    });
+    return fn.apply(null, args);
+  };
+
+  function register (func) {
+
+  };
+
+  module.exports = {
+    inject: inject,
+    register: register
+  };
 })();
