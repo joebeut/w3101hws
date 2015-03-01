9cbdd197b2c7a9dfe11fec3845300b0456e80246
Joe Beutler <jtb2133@columbia.edu>
2015-02-28 23:55:44 -0500

    feat(DI.js): beginning work on part two

--- hw3/lib/di/DI.js
+++ hw3/lib/di/DI.js
@@ -1,6 +1,32 @@
-(function () {
+var module = (function () {
   'use strict';
 
-  module.exports = {};
+  var dependencies = {};
 
+  var inject = function (target) {
+    var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+    var FN_ARG_SPLIT = /,/;
+    var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+    var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+    var text = target.toString();
+    var args = text.match(FN_ARGS)[1].split(',');
+
+    target.apply(target, this.getDependencies(args));
+  };
+
+  var getDependencies = function(arr) {
+    var self = this;
+    return arr.map(function(value) {
+      return self.dependencies[value];
+    });
+  };
+
+  var register = function (name, dependency) {
+    dependencies[name] = dependency;
+  };
+
+  module.exports = {
+    inject: inject,
+    register: register
+  };
 })();
